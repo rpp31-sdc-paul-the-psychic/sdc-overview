23ea42c9b436c895ae60eceb6f5f86ffa6ec5056
Rick Kunz <rick.c.kunz@gmail.com>
2022-01-04 13:54:48 -0800

    Restored backup version of files

---
+++ database/mongoScripts.js
@@ -0,0 +1,24 @@
+
+// var db = connect("127.0.0.1:27017/sdc");
+let conn = new Mongo("127.0.0.1:27017");
+let db = conn.getDB("sdc");
+
+
+db.features.aggregate(
+  [
+    {$group: {
+      _id: "$product_id",
+       "features": {$push:{
+         feature: "$feature",
+          value: "$value"}
+        }
+      }
+    },
+    { $merge: {
+      into: "product",
+      on: "id",
+      whenMatched: "merge",
+      whenNotMatched: "discard"
+    }
+    }
+], { "allowDiskUse" : true })
