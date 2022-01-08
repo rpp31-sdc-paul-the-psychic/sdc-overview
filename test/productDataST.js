f98cf34980b70a99c94614e15468fad28426ec38
Rick Kunz <rick.c.kunz@gmail.com>
2022-01-07 16:29:43 -0800

    Added handling of /products endpoint to retrieve multiple products

--- test/productDataST.js
+++ test/productDataST.js
@@ -1,17 +1,27 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+// const app = require('../server/index.js');
+// const db = require('../database/index.js');
 
-export const options = {
-  vus: 10,
-  duration: '30s',
-};
+// export const options = {
+//   vus: 10,
+//   duration: '30s',
+// };
+
+const min = Math.ceil(900000);
+const max = Math.floor(1000000);
+const id = Math.floor(Math.random() * (max - min) + min);
+
+const BASE_URL = `http://localhost:3000/products/${id}`;
 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+  // http.get('https://test.k6.io');
+  // sleep(1);
+
+  //test a range of ids - Math.random for last 10% of ids
 
 
   //for time on an individual request - comment out lines 4-7 and 10-11
-  // const res = http.get('http://httpbin.org');
-  // console.log('Response time was ' + String(res.timings.duration) + ' ms');
+  const res = http.get(BASE_URL);
+  console.log('Response time was ' + String(res.timings.duration) + ' ms');
 }
